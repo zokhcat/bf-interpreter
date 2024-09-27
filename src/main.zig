5e5173ec3f22885f50fee51ebb7cd9ce6b56042f
zokhcat <understandingzak@gmail.com>
2024-09-27 20:29:11 +0530

    feat(loops): Implement loop

--- src/main.zig
+++ src/main.zig
@@ -7,10 +7,11 @@ fn interpret(code: []const u8, allocator: std.mem.Allocator) !void {
     defer allocator.free(memory);
     @memset(memory, 0);
 
+    var instruction_pointer: usize = 0;
     var data_pointer: usize = 0;
 
-    for (code) |command| {
-        switch (command) {
+    while (instruction_pointer < code.len) : (instruction_pointer += 1) {
+        switch (code[instruction_pointer]) {
             '>' => {
                 if (data_pointer < MEMORY_SIZE - 1) {
                     data_pointer += 1;
@@ -23,7 +24,35 @@ fn interpret(code: []const u8, allocator: std.mem.Allocator) !void {
             },
             '+' => memory[data_pointer] +%= 1,
             '-' => memory[data_pointer] -%= 1,
-            else => {}, // Ignore other characters
+            '[' => {
+                if (memory[data_pointer] == 0) {
+                    var loop_depth: usize = 1;
+                    while (loop_depth > 0) : (instruction_pointer += 1) {
+                        instruction_pointer += 1;
+                        if (instruction_pointer >= code.len) break;
+                        switch (code[instruction_pointer]) {
+                            '[' => loop_depth += 1,
+                            ']' => loop_depth -= 1,
+                            else => {},
+                        }
+                    }
+                }
+            },
+            ']' => {
+                if (memory[data_pointer] != 0) {
+                    var loop_depth: usize = 1;
+                    while (loop_depth > 0) : (instruction_pointer -= 1) {
+                        if (instruction_pointer == 0) break;
+                        instruction_pointer -= 1;
+                        switch (code[instruction_pointer]) {
+                            ']' => loop_depth += 1,
+                            '[' => loop_depth -= 1,
+                            else => {},
+                        }
+                    }
+                }
+            },
+            else => {},
         }
     }
 
